9fe53696cbf5e47524ce43860e5f6b86b30860d1
xiduzo <mail@sanderboer.nl>
2026-03-17 09:47:18 +0100

    added UI components and hooks

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,16 @@
 import { Button } from "@/components/ui/button"
+import { Checkbox } from "@/components/ui/checkbox"
 
 export default function Page() {
   return (
     <div className="flex min-h-svh p-6">
       <div className="flex max-w-md min-w-0 flex-col gap-4 text-sm leading-loose">
         <div>
-          <h1 className="font-medium">Project ready!</h1>
+          <h1 className="font-medium">Welcome to my project!</h1>
           <p>You may now add components and start building.</p>
           <p>We&apos;ve already added the button component for you.</p>
           <Button className="mt-2">Button</Button>
+          <Checkbox />
         </div>
         <div className="font-mono text-xs text-muted-foreground">
           (Press <kbd>d</kbd> to toggle dark mode)
